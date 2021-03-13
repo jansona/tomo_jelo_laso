025a07121527aea59ed041372f4643e1a1fb1856
jansona <jan_sona@outlook.com>
2021-03-13 18:43:34 +0800

    feat: complete RSA module

--- encode_decode/sample_rsa.py
+++ encode_decode/sample_rsa.py
@@ -3,4 +3,71 @@ import random
 
 class RSA(object):
     
-    pass
+    def generate_key(self, width, seed=None):
+        """
+        the RSA key generating algorithm
+        k is the number of bits in n
+        """
+
+        def modinv(a, m):
+            """calculate the inverse of a mod m
+            that is, find b such that (a * b) % m == 1"""
+            b = 1
+            while not (a * b) % m == 1:
+                b += 1
+            return b
+
+        def gen_prime(k, seed=None):
+            """generate a prime with k bits"""
+
+            def is_prime(num):
+                if num == 2:
+                    return True
+                for i in range(2, int(num ** 0.5) + 1):
+                    if num % i == 0:
+                        return False
+                return True
+
+            random.seed(seed)
+            while True:
+                key = random.randrange(int(2 ** (k - 1)), int(2 ** k))
+                if is_prime(key):
+                    return key
+
+        # size in bits of p and q need to add up to the size of n
+        p_size = width / 2
+        q_size = width - p_size
+
+        e = gen_prime(width, seed)  # in many cases, e is also chosen to be a small constant
+
+        while True:
+            p = gen_prime(p_size, seed)
+            if p % e != 1:
+                break
+
+        while True:
+            q = gen_prime(q_size, seed)
+            if q % e != 1:
+                break
+
+        n = p * q
+        l = (p - 1) * (q - 1)  # calculate totient function
+        d = modinv(e, l)
+
+        return int(n), int(e), int(d)
+
+    def encrypt(data, e, n):
+        return pow(int(data), int(e), int(n))
+
+    def decrypt(data, d, n):
+        return pow(int(data), int(d), int(n))
+
+
+if __name__ == "__main__":
+    rsa = RSA()
+    n,e,d = rsa.generate_key(16)
+    data = 25566
+    encrypted = pow(data,e,n)
+    print(encrypted)
+    decrypted = pow(encrypted,d,n)
+    assert decrypted == data
